18f0a8d3f52aaacd2f8806978420a9a2ca17c0b4
Andres Villarroel <andres121231@gmail.com>
2024-02-20 11:09:58 -0500

    Initial Commit

---
+++ WheresMyStuff/WheresMyStuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheresMyStuffApp.swift
+//  WheresMyStuff
+//
+//  Created by Andres Villarroel on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct WheresMyStuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
