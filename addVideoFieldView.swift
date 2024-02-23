e71a97cae009512e5817c625a8abe8d3b05c3456
Andres Villarroel <andres121231@gmail.com>
2024-02-22 22:47:46 -0500

    Locked app orientation to portrait. Gave scrollview in BrowseView rounded corners. Built basic video guidance feature. Refactored the add photos and videos form fields in AddItemView to their own respective view files.

---
+++ addVideoFieldView.swift
@@ -0,0 +1,18 @@
+//
+//  AddVideoFieldView.swift
+//  WheresMyStuff
+//
+//  Created by Andres Villarroel on 2/22/24.
+//
+
+import SwiftUI
+
+struct AddVideoFieldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddVideoFieldView()
+}
