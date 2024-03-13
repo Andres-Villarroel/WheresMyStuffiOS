2bdf0f2b521325c8770066afcabc629a88217d92
Andres Villarroel <andres121231@gmail.com>
2024-03-12 21:40:42 -0400

    Save point for fixing the "Modifying state..." warning

---
+++ WheresMyStuff/View/FormView.swift
@@ -0,0 +1,18 @@
+//
+//  FormView.swift
+//  WheresMyStuff
+//
+//  Created by Andres Villarroel on 3/12/24.
+//
+
+import SwiftUI
+
+struct FormView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FormView()
+}
